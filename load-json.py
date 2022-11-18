4239aaccc164b3fe0903ed1bebb9c43b876cb13e
Arjun <arjun.mehta1001@gmail.com>
2022-11-18 10:54:59 -0700

    part 4 most likely complete: TODO: check if None is null/how tf that works

--- load-json.py
+++ load-json.py
@@ -1,19 +1,15 @@
 import json
 from pymongo import MongoClient
+from phase2 import Phase2
 
-# For this part, you will write a program, named load-json with a proper extension 
-# (e.g. load-json.py if using Python), which will take a json file in the current directory
-# and constructs a MongoDB collection. Your program will take as input a json file name and 
-# a port number under which the MongoDB server is running, will connect to the server and will 
-# create a database named 291db (if it does not exist). Your program then will create a collection named dblp.
-#  If the collection exists, your program should drop it and create a new collection. 
-# Your program for this phase ends after building the collection.
 
-def main():
-
+# TODO: checking if valid
+def phase1():
+    print("PHASE 1")
+    print("----------------------")
     port = input("Enter port: ")
-    # client = MongoClient('mongodb://localhost:27012')
-    client = MongoClient('mongodb://localhost:' + port)
+    client = MongoClient('mongodb://localhost:27012')
+    # client = MongoClient('mongodb://localhost:' + port)
     
     json_file = input("Enter JSON file: ")
 
@@ -22,12 +18,16 @@ def main():
 
     collection = db["dblp"]
     collection.drop()
-    # with open("dblp-ref-10.json") as f:
-    with open(json_file, 'r') as f:
+    with open("dblp-ref-10.json") as f:
+    # with open(json_file, 'r') as f:
         # data = json.load(f.read())
         for line in f:
             data = json.loads(line)
             collection.insert_one(data)
+    
 
 if __name__ == "__main__":
-    main()
+    phase1()
+    test = Phase2()
+    test.run()
+
---
+++ phase2.py
@@ -0,0 +1,95 @@
+from pymongo import MongoClient
+
+class Phase2:
+    def __init__(self):
+        pass
+    
+    def print_gabagool(self):
+        print(self.port)
+    
+    def handle_1(self):
+        pass
+    
+    def handle_2(self):
+        pass
+    
+    def handle_3(self):
+        pass
+    
+
+
+    # The user should be able to add an article to the collection by providing a unique id, 
+    # a title, a list of authors, and a year. The fields abstract and venue should be set to null, 
+    # references should be set to an empty array and n_citations should be set to zero.
+
+    def handle_4(self):
+        id = input("Enter Unique id_ ")
+
+        while(self.collection.count_documents({'id': id}) > 0):
+            id = input("id already exists. Please enter a new id or EXIT to return to menu._ ")
+            if id == "EXIT":
+                return
+                
+
+        title = input("Enter title_ ")
+        author_list = []
+
+        while(True):
+            new_author = input("Enter name of new author or 'EXIT' to stop adding authors_ ")
+            if new_author == "EXIT":
+                break
+            else:
+                author_list.append(new_author)
+        
+        year = int(input("Enter year_ "))
+        while(year < 0):
+            year = (input("year must be above 0. Please enter a new year or EXIT to return to menu._ "))
+            if id == "EXIT":
+                return
+        
+        # TODO: verify None is null
+        dict_document = {
+            "abstract": None,
+            "authors": author_list,
+            "n_citation": 0,
+            "references": [],
+            "title": title,
+            "venue": "",
+            "year": year,
+            "id": id
+        }
+        self.collection.insert_one(dict_document)
+
+
+
+    def run(self):
+        self.port = input("Enter port: ")
+        client = MongoClient('mongodb://localhost:27012')
+        # client = MongoClient('mongodb://localhost:' + port)
+
+        self.db = client["291db"]
+        self.collection = self.db["dblp"]
+
+        while(True):
+            print("Choose from the following options:")
+            print("1. Search for articles")
+            print("2. Search for authors")
+            print("3. List the venues")
+            print("4. Add an article")
+            print("5. Exit")
+            choice = input("Enter Choice_ ")
+
+            if(choice == "1"):
+                self.handle_1()
+            elif(choice == "2"):
+                self.handle_2()
+            elif(choice == "3"):
+                self.handle_3()
+            elif(choice == "4"):
+                self.handle_4()
+            elif(choice == "5"):
+                print("Exiting system!")
+                break
+            else:
+                print("Invalid choice. Please choose again.")
+
